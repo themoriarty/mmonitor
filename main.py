5d4933d6d6acfc7ecbabeb3325f1adccc8e8f8ce
Morarenko Kirill <m0riarty@ya.ru>
2012-11-14 21:06:51 -0800

    groups

--- main.py
+++ main.py
@@ -21,19 +21,22 @@ def post(url, data, headers):
     return b
 
 def main():
-    balances = []
-    specials = []
+    balances = {}
     for request in config.requests:
         data = open(request["file"]).read() % request
         response_data = post(request["url"], data, { "Content-type": "application/x-ofx",
                                                      "Accept": "*/*, application/x-ofx"
                                                      })
         response = OfxParser.parse(response_data)
-        balances.append(sum([float(x.statement.balance) for x in response.accounts]))
-        for label, k in request.get("special", {}).items():
-            balance = [a.statement.balance for a in response.accounts if a.number == request[k]]
-            if balance:
-                specials.append("%s: %d" % (label, balance[0]))
+        groups = dict([(request[k], v) for k, v in request.get("groups", {}).items()])
+        for account in response.accounts:
+            group = groups.get(account.number, "")
+            balances[group] = balances.get(group, 0) + account.statement.balance
+        #balances.append(sum([float(x.statement.balance) for x in response.accounts]))
+        #for label, k in request.get("special", {}).items():
+        #    balance = [a.statement.balance for a in response.accounts if a.number == request[k]]
+        #    if balance:
+        #        specials.append("%s: %d" % (label, balance[0]))
 #         positive_accounts = set([request[x.strip()] for x in request.get("positive", "").split(", ") if x.strip()])
 #         positive = sum([float(x.statement.balance) for x in response.accounts if x.number in positive_accounts])
 #         negative = sum([float(x.statement.balance) for x in response.accounts if x.number not in positive_accounts])
@@ -43,6 +46,6 @@ def main():
         #balance = ch + c
         #print "%d %d %dk" % (balance, zch, s / 1000)
         #print "%d %d" % (balance, zch)
-    print "%d %s" % (sum(balances) - 10000, ", ".join(specials))
+    print "%d %s" % (balances[""] - 10000, ", ".join(["%s: %d" % (k, v) for k, v in balances.items() if k]))
 if __name__ == "__main__":
     main()
